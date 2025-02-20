ca359d11dbbaf3c0b8e87aee29ab557428600da0
mari <_mari@tuta.io>
2025-02-20 14:35:40 +0800

    lib(bevy-compositor): add convenient calloop type aliases

--- crates/bevy-compositor/src/lib.rs
+++ crates/bevy-compositor/src/lib.rs
@@ -3,18 +3,23 @@ use bevy::app::PluginsState;
 use bevy::prelude::*;
 use bevy::render::extract_resource::ExtractResourcePlugin;
 use external_image::ExternalImagePlugin;
-use smithay::reexports::calloop::EventLoop;
+use smithay::reexports::calloop;
 
 pub mod convert;
 pub mod external_image;
 pub mod state;
 pub mod util;
 
+pub(crate) use self::state::SmithayAppRunnerState as State;
+
+pub(crate) type EventLoop = calloop::EventLoop<'static, State>;
+pub(crate) type LoopHandle = calloop::LoopHandle<'static, State>;
+
 pub struct SmithayPlugin;
 
 impl Plugin for SmithayPlugin {
     fn build(&self, app: &mut App) {
-        let event_loop = match EventLoop::<SmithayAppRunnerState>::try_new() {
+        let event_loop = match EventLoop::try_new() {
             Ok(event_loop) => event_loop,
             Err(error) => {
                 error!("failed to create event loop: {error}");
@@ -37,13 +42,13 @@ pub fn smithay_runner(mut app: App) -> AppExit {
 
     let mut event_loop = app
         .world_mut()
-        .remove_non_send_resource::<EventLoop<SmithayAppRunnerState>>()
+        .remove_non_send_resource::<EventLoop>()
         .unwrap();
 
     app.world_mut()
         .insert_non_send_resource(event_loop.handle());
 
-    let mut runner_state = match SmithayAppRunnerState::try_new(&mut event_loop, app) {
+    let mut runner_state = match State::try_new(&mut event_loop, app) {
         Ok(runner_state) => runner_state,
         Err(error) => {
             error!("{error}");
